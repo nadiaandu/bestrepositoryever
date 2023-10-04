05c2e7efe3aca690362f89416962a5cfe8b0fd47
nadia <nadiaandu7@gmail.com>
2023-10-04 10:00:56 -0500

    variables

---
+++ practice10.04.pdf/src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+    String personName = "John Doe";
+    Integer personAge = 25;
+    Double productPrice = 19.99;
+    Float temperatureCelsius =27.5f;
+    Boolean isLoggedIn = true;
+
+
+}
