2c6dd12b65e4f0699a06d19814475920b60b083b
nadia <nadiaandu7@gmail.com>
2023-11-05 16:36:16 -0600

    ..

--- VehicleDealership/src/main/java/org/example/UserInterface.java
+++ VehicleDealership/src/main/java/org/example/UserInterface.java
@@ -1,11 +1,13 @@
 package org.example;
 
+import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Scanner;
 
 
 public class UserInterface {
     public Dealership dealership;
+
     public UserInterface(Dealership dealership) {
         this.dealership = dealership;
     }
@@ -26,51 +28,133 @@ public class UserInterface {
             System.out.println("6. Search for vehicles by vehicle type");
             System.out.println("7. Add a vehicle");
             System.out.println("9. Remove a vehicle");
-            System.out.println("0 Quit application");
 
             int userInput = scanner.nextInt();
 
             switch (userInput) {
 
                 case 0:
-                    isRunning = false;
+                    System.exit(0);
                     break;
                 case 1:
-                    dealership.getAllVehicles();
+
                     break;
                 case 2:
-                    dealership.getVehiclesByPrice();
+
                     break;
                 case 3:
-                    dealership.getVehiclesByMakeModel();
+
                     break;
                 case 4:
-                    dealership.getVehiclesByYear();
+
                     break;
                 case 5:
-                    dealership.getVehiclesByColor();
+
                     break;
                 case 6:
-                    dealership.getVehiclesByType();
+
                     break;
                 case 7:
-                    dealership.addVehicle();
+
                     break;
 
                 case 8:
-                    dealership.removeVehicle();
+
                     break;
                 default:
-                    System.out.println();
+                    System.out.println("Please select from options given!");
             }
             scanner.close();
 
         }
         // methods for user
-
-
-
-
-
     }
+
+    private void listAllVehicles() {
+        List<Vehicle> allVehicles = dealership.getAllVehicles();
+        displayFilteredVehicles(allVehicles);
+    }
+
+    private void displayFilteredVehicles(List<Vehicle> filteredVehicles) {
+    }
+
+    private void addVehicle() {
+        System.out.println("Please answer the following to add a vehicle");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter VIN: ");
+        int vin = scanner.nextInt();
+        System.out.print("Enter the vehicle's year: ");
+        int year = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter the vehicle's make: ");
+        String make = scanner.nextLine();
+        System.out.print("Enter the vehicle's model: ");
+        String model = scanner.nextLine();
+        System.out.print("Enter the vehicle type (car, truck, SUV, van): ");
+        String vehicleType = scanner.nextLine();
+        System.out.print("Enter the vehicle's color: ");
+        String color = scanner.nextLine();
+        System.out.print("Enter the vehicle's odometer: ");
+        int odometer = scanner.nextInt();
+        System.out.print("Enter the vehicle's price: ");
+        double price = scanner.nextDouble();
+
+        Vehicle newVehicle = new Vehicle(vin, year, make, model, vehicleType, color, odometer, price);
+        dealership.addVehicle(newVehicle);
+        System.out.println("Successfully added!");
+    }
+
+    private void searchByPrice() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter your min price");
+        double minPrice = userInput.nextDouble();
+        System.out.println("Enter your max price");
+        double maxPrice = userInput.nextDouble();
+
+        List<Vehicle> filteredVehicles = dealership.getVehiclesByPrice(minPrice, maxPrice);
+        displayFilteredVehicles(filteredVehicles);
+    }
+
+    private void searchByMakeModel() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter make");
+        String make = userInput.nextLine();
+        System.out.println("Enter model");
+        String model = userInput.nextLine();
+
+        List<Vehicle> filteredVehicles = dealership.getVehiclesByMakeModel(make, model);
+        displayFilteredVehicles(filteredVehicles);
+    }
+
+    private void searchByYear() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter min year");
+        int minYear = userInput.nextInt();
+        System.out.println("Enter max year");
+        int maxYear = userInput.nextInt();
+
+        List<Vehicle> filteredVehicles = dealership.getVehiclesByYear(minYear, maxYear);
+        displayFilteredVehicles(filteredVehicles);
+    }
+
+    private void searchByColor() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter desired vehicle color");
+        String color = userInput.nextLine();
+
+        List<Vehicle> filteredVehicles = dealership.getVehiclesByColor(color);
+        displayFilteredVehicles(filteredVehicles);
+    }
+
+    private void searchByType() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter desired vehicle type");
+        String type = userInput.nextLine();
+
+
+        List<Vehicle> filteredVehicles = dealership.getVehiclesByType(type);
+        displayFilteredVehicles(filteredVehicles);
+    }
+
+
 }
